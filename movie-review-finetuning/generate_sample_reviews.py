86b769bce80a8293677436ecd8eb690fe1ce5d17
Samia Zaman <samiazaman@Samias-MacBook-Pro.local>
2025-02-10 23:31:13 -0500

    Successfully loaded state dictionary with averaged weights

---
+++ movie-review-finetuning/generate_sample_reviews.py
@@ -0,0 +1,189 @@
+import torch
+from tqdm import tqdm
+import pandas as pd
+
+tqdm.pandas()
+
+from transformers import pipeline, AutoTokenizer
+from datasets import load_dataset
+
+from trl import PPOTrainer, PPOConfig, AutoModelForCausalLMWithValueHead
+from trl.core import LengthSampler
+import wandb
+
+class Generate:
+    # set review type
+    def __init__(self, review_type):
+        wandb.init()
+        self.review_type = review_type
+
+    def run(self):
+
+        # Change based on positive/negative/neutral movie review type
+        model_name_to_save = "gpt2-imdb-pos-v2"
+        base_model_name = "carolinezhang/gpt2-imdb-pos-v2"
+        if self.review_type == "negative":
+            model_name_to_save = "gpt2-imdb-neg-v2"
+            base_model_name = "Samzy17/gpt2-imdb-movie-reviews-negative"
+        elif self.review_type == "neutral":
+            model_name_to_save = "gpt2-imdb-neutral-v2"
+            base_model_name = "Samzy17/gpt2-imdb-pos-neg-averaged"
+
+        ref_model = AutoModelForCausalLMWithValueHead.from_pretrained("lvwerra/gpt2-imdb")
+        tokenizer = AutoTokenizer.from_pretrained("lvwerra/gpt2-imdb")
+        finetuned_model = AutoModelForCausalLMWithValueHead.from_pretrained(base_model_name)
+        
+        device = 0 if torch.cuda.is_available() else "cpu" 
+        sent_kwargs = {"top_k": None, "function_to_apply": "none", "batch_size": 16}
+        gen_kwargs = {
+            "min_length": -1,
+            "top_k": 0.0,
+            "top_p": 1.0,
+            "do_sample": True,
+            "pad_token_id": tokenizer.eos_token_id,
+        }
+
+        ### Load IMDB dataset
+        '''The IMDB dataset contains 50k movie review annotated with "positive"/"negative" feedback indicating the sentiment.  
+        We load the IMDB dataset into a DataFrame and filter for comments that are at least 200 characters. 
+        Then we tokenize each text and cut it to random size with the `LengthSampler`. '''
+
+        def build_dataset(model_name, dataset_name="stanfordnlp/imdb",input_min_text_length=2,input_max_text_length=8,):
+            """
+            Build dataset for training. This builds the dataset from `load_dataset`, one should
+            customize this function to train the model on its own dataset.
+
+            Args:
+                dataset_name (`str`):
+                    The name of the dataset to be loaded.
+
+            Returns:
+                dataloader (`torch.utils.data.DataLoader`):
+                    The dataloader for the dataset.
+            """
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            tokenizer.pad_token = tokenizer.eos_token
+            # load imdb with datasets
+            ds = load_dataset(dataset_name, split="train")
+            ds = ds.rename_columns({"text": "review"})
+            ds = ds.filter(lambda x: len(x["review"]) > 200, batched=False)
+
+            input_size = LengthSampler(input_min_text_length, input_max_text_length)
+
+            def tokenize(sample):
+                sample["input_ids"] = tokenizer.encode(sample["review"])[: input_size()]
+                sample["query"] = tokenizer.decode(sample["input_ids"])
+                return sample
+
+            ds = ds.map(tokenize, batched=False)
+            ds.set_format(type="torch")
+            return ds
+
+        dataset = build_dataset("lvwerra/gpt2-imdb")
+
+        ### Model Inspection
+        '''
+        Let's inspect some examples from the IMDB dataset. 
+        We can use ref_model to compare the finetuned model against the ref model before optimisation.
+        '''
+        #### get a batch from the dataset
+        bs = 16
+        game_data = dict()
+        dataset.set_format("pandas")
+        df_batch = dataset[:].sample(bs)
+        game_data["query"] = df_batch["query"].tolist()
+        query_tensors = df_batch["input_ids"].tolist()
+
+        response_tensors_ref, response_tensors = [], []
+
+        output_min_length = 4
+        output_max_length = 16
+        output_length_sampler = LengthSampler(output_min_length, output_max_length)
+
+        #### get response from gpt2 and gpt2_ref
+        for i in range(bs):
+            query = torch.tensor(query_tensors[i]).to(device)
+
+            gen_len = output_length_sampler()
+            query_response = ref_model.generate(
+                query.unsqueeze(0), max_new_tokens=gen_len, **gen_kwargs
+            ).squeeze()
+            response_len = len(query_response) - len(query)
+            response_tensors_ref.append(query_response[-response_len:])
+
+            query_response = finetuned_model.generate(
+                query.unsqueeze(0), max_new_tokens=gen_len, **gen_kwargs
+            ).squeeze()
+            response_len = len(query_response) - len(query)
+            response_tensors.append(query_response[-response_len:])
+
+        #### decode responses
+        game_data["response (before)"] = [
+            tokenizer.decode(response_tensors_ref[i]) for i in range(bs)
+        ]
+        game_data["response (after)"] = [
+            tokenizer.decode(response_tensors[i]) for i in range(bs)
+]
+
+        #### sentiment analysis of query/response pairs before/after
+        sentiment_pipe = pipeline(
+            "sentiment-analysis", model="lvwerra/distilbert-imdb", device=device
+        )
+
+        # ##### Results of reference model
+        texts = [q + r for q, r in zip(game_data["query"], game_data["response (before)"])]
+        pipe_outputs = sentiment_pipe(texts, **sent_kwargs)
+
+        positive_scores = [
+            item["score"]
+            for output in pipe_outputs
+            for item in output
+            if item["label"] == "POSITIVE"
+        ]
+
+        negative_scores = [
+            item["score"]
+            for output in pipe_outputs
+            for item in output
+            if item["label"] == "NEGATIVE"
+        ]
+        game_data["positive rewards (before)"] = positive_scores
+        game_data["negative rewards (before)"] = negative_scores
+
+        # ##### Results of finetuned model
+        texts = [q + r for q, r in zip(game_data["query"], game_data["response (after)"])]
+        pipe_outputs = sentiment_pipe(texts, **sent_kwargs)
+
+        positive_scores = [
+            item["score"]
+            for output in pipe_outputs
+            for item in output
+            if item["label"] == "POSITIVE"
+        ]
+
+        negative_scores = [
+            item["score"]
+            for output in pipe_outputs
+            for item in output
+            if item["label"] == "NEGATIVE"
+        ]
+
+        game_data["positive rewards (after)"] = positive_scores
+        game_data["negative rewards (after)"] = negative_scores
+
+        # store results in a dataframe
+        df_results = pd.DataFrame(game_data)
+        df_results
+
+        print("mean:")
+        print(df_results[["positive rewards (before)", "positive rewards (after)"]].mean())
+        print(df_results[["negative rewards (before)", "negative rewards (after)"]].mean())
+        print()
+        print("median:")
+        print(df_results[["positive rewards (before)", "positive rewards (after)"]].median())
+        print(df_results[["negative rewards (before)", "negative rewards (after)"]].median())
+
+if __name__ == "__main__":
+
+    ft_gpt2 = Generate("neutral")  # positive, negative, neutral
+    ft_gpt2.run()
--- movie-review-finetuning/interpolation.py
+++ movie-review-finetuning/interpolation.py
@@ -1,25 +1,50 @@
 from transformers import AutoModel, AutoTokenizer
 from trl import AutoModelForCausalLMWithValueHead
+from collections import OrderedDict
 
-model_name = "carolinezhang/gpt2-imdb-pos"
+# model1 
+pos_model_name = "carolinezhang/gpt2-imdb-pos"
+pos_model = AutoModelForCausalLMWithValueHead.from_pretrained(pos_model_name)
+pos_tokenizer = AutoTokenizer.from_pretrained(pos_model_name)
 
-model = AutoModelForCausalLMWithValueHead.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model 2
+neg_model_name = "Samzy17/gpt2-imdb-movie-reviews-negative"
+neg_model = AutoModelForCausalLMWithValueHead.from_pretrained(neg_model_name)
+neg_tokenizer = AutoTokenizer.from_pretrained(neg_model_name)
 
-# print(model.state_dict().keys())
-# for layer_name in model.state_dict().keys(): 
-#     weights = model.state_dict()[layer_name].detach().numpy()
-#     print(layer_name)
-#     print(weights)
+# verify size of state dictionary:
+print(f'Pos state dict size = {len(pos_model.state_dict().items())}, 
+      Neg state dict size = {len(neg_model.state_dict().items())}')
 
-first_key = list(model.state_dict().keys())[0]
-test_avg = 0.5*(model.state_dict()[first_key].detach().numpy()) + 0.5*(model.state_dict()[first_key].detach().numpy())
-print(test_avg)
+# Model whose weights to update
+base_model_name = "lvwerra/gpt2-imdb"
+base_model = AutoModelForCausalLMWithValueHead.from_pretrained(base_model_name)
+base_model_tokenizer = AutoTokenizer.from_pretrained(base_model_name)
 
-model.state_dict()[first_key] = "new value"
-print(model.state_dict()[first_key])
+# State dictionary to contain the average weights
+base_model_sd = OrderedDict()
 
-prompt = "This movie was"
-inputs = tokenizer(prompt, return_tensors="pt")
-outputs = model.generate(**inputs, max_length=50)
-print(tokenizer.decode(outputs[0]))
+# i = 0
+# Populate the state dictionary to be loaded onto the base model
+for k,v in neg_model.state_dict().items():
+    # print(f'key {i} ={k}, value={v}')
+    val1 = pos_model.state_dict()[k]
+    val2 = neg_model.state_dict()[k]
+    
+    # Weights that should NOT have the 'pretrained' prefix included
+    no_pretrained = ['v_head.summary.weight', 'v_head.summary.bias']
+    if k in no_pretrained:
+        # average weights!
+        base_model_sd[k] = (0.5 * val1) + (0.5 * val2)
+    else:
+        # Weights that SHOULD have the 'pretrained' prefix included
+        # average weights!
+        base_model_sd[f'pretrained_model.{k}'] = (0.5 * val1) + (0.5 * val2)
+    # i +=1
+
+# LOAD newly populated state dict to the base model 
+base_model.load_state_dict(base_model_sd)  # should print "<All keys matched successfully>"
+
+# SAVE model with new state dict locally (upload to hf if desired)
+base_model_tokenizer = AutoTokenizer.from_pretrained(base_model_name)
+base_model_tokenizer.save_pretrained('gpt2-imdb-pos-neg-interpolated')
