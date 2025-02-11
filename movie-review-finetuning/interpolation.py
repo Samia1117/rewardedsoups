fc86b4fbc3adfe8c0da5401dacf79b05553019a3
Caroline Zhang <czcz2005@gmail.com>
2025-02-10 23:55:54 -0500

    avg model

--- movie-review-finetuning/interpolation.py
+++ movie-review-finetuning/interpolation.py
@@ -1,10 +1,8 @@
 from transformers import AutoModel, AutoTokenizer
 from trl import AutoModelForCausalLMWithValueHead
 
-model_name = "carolinezhang/gpt2-imdb-pos"
-
-model = AutoModelForCausalLMWithValueHead.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+pos_model_name = "carolinezhang/gpt2-imdb-pos"
+neg_model_name = "Samzy17/gpt2-imdb-movie-reviews-negative"
 
 # print(model.state_dict().keys())
 # for layer_name in model.state_dict().keys(): 
@@ -12,16 +10,31 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 #     print(layer_name)
 #     print(weights)
 
-first_key = list(model.state_dict().keys())[0]
-test_avg = 0.5*(model.state_dict()[first_key].detach().numpy()) + 0.5*(model.state_dict()[first_key].detach().numpy())
-print("test average:")
-print(test_avg)
+pos_model = AutoModelForCausalLMWithValueHead.from_pretrained(pos_model_name)
+pos_tokenizer = AutoTokenizer.from_pretrained(pos_model_name)
 
-model.state_dict()[first_key] = "new value"
-print("modified value of first key:")
-print(model.state_dict()[first_key])
+neg_model = AutoModelForCausalLMWithValueHead.from_pretrained(neg_model_name)
+neg_tokenizer = AutoTokenizer.from_pretrained(neg_model_name)
+
+pos_state_dict = pos_model.state_dict()
+neg_state_dict = neg_model.state_dict()
+
+avg_state_dict = {}
+
+for key in pos_state_dict.keys():
+    pos_weights = pos_state_dict[key]  
+    neg_weights = neg_state_dict[key]  
+
+    avg_weights = 0.5*pos_weights + 0.5*neg_weights
+
+    avg_state_dict[key] = avg_weights
+
+#using the pos model, either should work bc they have the same architecture
+avg_model = AutoModelForCausalLMWithValueHead.from_pretrained(pos_model_name)
+
+avg_model.load_state_dict(avg_state_dict)
 
 prompt = "This movie was"
-inputs = tokenizer(prompt, return_tensors="pt")
-outputs = model.generate(**inputs, max_length=50)
-print(tokenizer.decode(outputs[0]))
+inputs = pos_tokenizer(prompt, return_tensors="pt")
+outputs = avg_model.generate(**inputs, max_length=50)
+print(pos_tokenizer.decode(outputs[0]))
